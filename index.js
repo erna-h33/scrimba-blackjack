5cd28a04c14a05215f60e80c87235d2d94405d31
erna-h33 <erna.halim33@gmail.com>
2024-11-09 10:42:11 +1100

    Daily commit

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ function startGame() {
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -32,5 +36,6 @@ function renderGame() {
 function newCard() {
   let card = 6;
   sum += card;
+  cards.push(card);
   renderGame();
 }
